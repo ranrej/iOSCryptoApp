aa054170db0fea93f96ae774608c34bb76d1f48e
ranrej <rania.rejdal@ontariotechu.net>
2023-12-26 19:33:34 -0500

    Creating HomeView and a navigation header

---
+++ CryptoApp/CryptoApp/Core/Home/Views/HomeView.swift
@@ -0,0 +1,61 @@
+//
+//  HomeView.swift
+//  CryptoApp
+//
+//  Created by Rania Rejdal on 2023-12-26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // Background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // Content layer
+            VStack {
+                homeHeader
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        HomeView()
+            .navigationBarHidden(true)
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
