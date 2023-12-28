2c683f089cef6271a3b733f0099f3daaeabc5d01
ranrej <rania.rejdal@ontariotechu.net>
2023-12-28 00:50:57 -0500

    Add a dynamic List to display downloaded coins

---
+++ CryptoApp/CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewModel.swift
+//  CryptoApp
+//
+//  Created by Rania Rejdal on 2023-12-28.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+}
--- CryptoApp/CryptoApp/Core/Home/Views/HomeView.swift
+++ CryptoApp/CryptoApp/Core/Home/Views/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @EnvironmentObject private var vm: HomeViewModel
     @State private var showPortfolio: Bool = false
     
     var body: some View {
@@ -20,18 +21,39 @@ struct HomeView: View {
             // Content layer
             VStack {
                 homeHeader
+                
+                if !showPortfolio {
+                    allCoinsList
+                        .transition(.move(edge: .leading))
+                }
+                if showPortfolio {
+                    portfolioCoinsList
+                        .transition(.move(edge: .trailing))
+                }
+                
                 Spacer(minLength: 0)
             }
         }
     }
 }
 
-#Preview {
-    NavigationView {
-        HomeView()
-            .navigationBarHidden(true)
+struct HomeView_Preveiws: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+        .environmentObject(dev.homeVM)
     }
 }
+
+//#Preview {
+//    NavigationView {
+//        HomeView()
+//            .navigationBarHidden(true)
+//    }
+//    .environmentObject(dev.homeVM)
+//}
 
 extension HomeView {
     private var homeHeader: some View {
@@ -57,5 +79,25 @@ extension HomeView {
                 }
         }
         .padding(.horizontal)
+    }
+    
+    private var allCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 0, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(vm.portfolioCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 0, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
     }
 }
