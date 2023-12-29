395b0cb9fc19b0d598d5a0c03e22f3876ee14df5
ranrej <rania.rejdal@ontariotechu.net>
2023-12-28 21:17:57 -0500

    Creating a data model and View to display market statistics

--- CryptoApp/CryptoApp/Core/Home/Views/HomeView.swift
+++ CryptoApp/CryptoApp/Core/Home/Views/HomeView.swift
@@ -21,7 +21,7 @@ struct HomeView: View {
             // Content layer
             VStack {
                 homeHeader
-                
+                HomeStatsView(showPortfolio: $showPortfolio)
                 SearchBarView(searchText: $vm.searchText)
                 
                 columnTitles
